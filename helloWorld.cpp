d7d5c5c1b3aae766dd4ff0c0592665772d56f4e3
Hao Wu <hao_wu@csil.cs.ucsb.edu>
2022-04-05 16:46:53 -0700

    finished lab1

---
+++ helloWorld.cpp
@@ -0,0 +1,10 @@
+// helloWorld.cpp  for UCSB CS32 S22
+// Edited by: Hao Wu
+// minimal Hello World! program for 
+
+#include <iostream>
+
+int main() {
+  std::cout << "Hello, World!" << std::endl;
+  return 0;
+}
